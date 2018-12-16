fcbb84b9ccae1ef0008015f4abcb95c923e06164
Deepnathalanki <45890793+Deepnathalanki@users.noreply.github.com>
2018-12-16 13:33:47 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,31 +1,107 @@
-package com.asworld.techie.team5;
+package e.naveena.team5;
 
-import android.content.Intent;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.EditText;
+import android.view.View;
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button wm_btn,hni_btn;
+
+
+    private TextView mDateDisplay;
+    private Button PickDate;
+EditText hrs,mins;
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+    private int mHour;
+    private int mMinute;
+    static final int DATE_DIALOG_ID = 1111;
+    static final int TIME_DIALOG_ID = 2222;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        wm_btn= (Button)findViewById(R.id.wm_btn);
-        hni_btn=(Button)findViewById(R.id.hni_btn);
-        wm_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,WealthManager.class));
-            }
-        });
-        hni_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,Hni.class));
+        setContentView(R.layout.activity_hniprofile);
+
+        // capture our View elements
+        mDateDisplay = (TextView) findViewById(R.id.dateDisplay);
+        PickDate = (Button) findViewById(R.id.pickDate);
+
+        // add a click listener to the button
+        PickDate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showDialog(DATE_DIALOG_ID);
+
             }
         });
 
+        // get the current date
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        // display the current date (this method is below)
+        updateDisplay();
     }
+
+    @Deprecated
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_DIALOG_ID:
+                return new DatePickerDialog(this,
+                        mDateSetListener,
+                        mYear, mMonth, mDay);
+
+                case TIME_DIALOG_ID:
+                    return new TimePickerDialog(this,
+                            mTimeSetListener, mHour, mMinute, false);
+
+        }
+        return null;
+    }
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener =
+            new TimePickerDialog.OnTimeSetListener() {
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    mHour = hourOfDay;
+                    mMinute = minute;
+                    updateDisplay();
+                }
+            };
+    // updates the date we display in the TextView
+    private void updateDisplay() {
+        mDateDisplay.setText(
+                new StringBuilder()
+                        // Month is 0 based so add 1
+                        .append(mMonth + 1).append("-")
+                        .append(mDay).append("-")
+                        .append(mYear).append(" "));
+    }
+
+    // the callback received when the user "sets" the date in the dialog
+    private DatePickerDialog.OnDateSetListener mDateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+                public void onDateSet(DatePicker view, int year,
+                                      int monthOfYear, int dayOfMonth) {
+                    mYear = year;
+                    mMonth = monthOfYear;
+                    mDay = dayOfMonth;
+                    updateDisplay();
+                }
+            };
 }
